61aa216eef0f0e7ee64a6646ee401dc1049ddea4
EllyKurgat <ellykurgat00@gmail.com>
2026-03-13 16:42:52 +0300

    Fix template path in settings

--- RMS/settings.py
+++ RMS/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'RMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [BASE_DIR / 'frontend_views' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
